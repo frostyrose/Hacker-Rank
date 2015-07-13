7967408550532e4ce6515ba3a4899b9c0e5b63ea
Cameron Jones <ctjones@wpi.edu>
2015-07-12 22:19:11 -0400

    Completed Minimum Draws Challenge
    Implemented in Python 2 - Full Points

---
+++ Python/Math-Domain/minimumDraws.py
@@ -0,0 +1,7 @@
+# Python 2
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+numTests = raw_input()
+while numTests:
+    numSockPairs = raw_input()
+    numSocksRequired = (int(numSockPairs)) + 1
+    print numSocksRequired
