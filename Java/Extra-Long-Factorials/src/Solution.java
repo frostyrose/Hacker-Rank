9f701c066da6bbea2015d90fcabee532bf64e44a
Cameron Jones <ctjones@wpi.edu>
2015-07-14 23:11:51 -0400

    Completed Extra Long Factorials Challenge
    Corrected the Input Stream to read using scanner - Full Points

--- Java/Extra-Long-Factorials/src/Solution.java
+++ Java/Extra-Long-Factorials/src/Solution.java
@@ -8,19 +8,13 @@ public class Solution {
 
     public static void main(String[] args) throws IOException {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        InputStreamReader cin = new InputStreamReader(System.in);
-        int num = 0; //
-        try{
-            num = cin.read();//Only Gives First ByteValue
-            System.out.println(Integer.toString(num));
-        }catch (IOException ex) {
-            throw ex;
-        }
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+        num = sc.nextInt();
         BigInteger n = new BigInteger("1");
         for(int i = 2; i <= num; i++){
             n = n.multiply(new BigInteger(Integer.toString(i)));
         }
-        System.out.println(Integer.toString(num));
         System.out.println(n.toString());
     }
 }
