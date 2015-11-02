d967dd9535adf2dfe5e252f332547e69a052a053
Cameron Jones <ctjones@wpi.edu>
2015-11-02 11:40:33 -0500

    Corrected to take into account negative numbers
    Now fully correct

--- Java/Java-Domain/Data-Structures/Java-2D-Array/solution.java
+++ Java/Java-Domain/Data-Structures/Java-2D-Array/solution.java
@@ -15,7 +15,7 @@ public class Solution {
                 myArray[i][j] = sc.nextInt();
             }
         }
-        int max = 0;
+        int max = -9*7;
         for(int i = 0; i<4; i++){
             for(int j = 0; j<4 ; j++){
                 int sum = myArray[i][j] + myArray[i][j+1] + myArray[i][j+2] + myArray[i+1][j+1] + myArray[i+2][j] + myArray[i+2][j+1] + myArray[i+2][j+2];
