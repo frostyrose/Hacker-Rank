62a15d47f392c6abdb236f137fac35cc409477e9
Cameron Jones <ctjones@wpi.edu>
2015-11-02 10:24:45 -0500

    Completed Java Inheritance 2 Challenge
    Implemented in Java - Full Points

---
+++ Java/Java-Domain/Object-Oriented-Programming/Java-Inheritance-2/solution.java
@@ -0,0 +1,17 @@
+class Arithmetic {
+    
+    String getName(){
+        return "Arithmetic";
+    }    
+}
+
+class Adder extends Arithmetic {
+
+    String getName(){
+        return "Arithmetic";
+    }
+    
+    int add(int a, int b){
+        return a + b;
+    }
+}
