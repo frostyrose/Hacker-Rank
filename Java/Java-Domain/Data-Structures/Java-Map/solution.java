e3c8a2a088b5d6e73c202dca3ba781e4c8a8c17a
Cameron Jones <ctjones@wpi.edu>
2015-11-02 12:14:17 -0500

    Completed Java Map Challenge
    Implemented in Java - Full Points

---
+++ Java/Java-Domain/Data-Structures/Java-Map/solution.java
@@ -0,0 +1,30 @@
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+   public static void main(String []argh)
+   {
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+      in.nextLine(); //Throw away garbage new line
+      HashMap<String,Integer> myMap = new HashMap<String,Integer>();
+      for(int i=0;i<n;i++)
+      {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+         myMap.put(name,phone);
+         in.nextLine();
+      }
+      while(in.hasNext())
+      {
+         String s=in.nextLine();
+          if(myMap.containsKey(s)){
+              System.out.println(s + "=" + myMap.get(s));
+          }
+          else{
+              System.out.println("Not found");
+          }
+      }
+   }
+}
