9a02866ba716557ab664f00d9d42a8cf2bb5352b
Cameron Jones <ctjones@wpi.edu>
2015-11-02 01:16:40 -0500

    Completed Java End of File Challenge
    Implemented in Java - Full Points

---
+++ Java/Java-Domain/Introduction/Java-End-of-File/solution.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        int i = 1;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            System.out.printf("%d %s\n",i++ , sc.nextLine());
+        }
+    }
+}
