3e5f1b75aaca11df70e8a238edd937b953cf56c7
Cameron Jones <ctjones@wpi.edu>
2015-11-04 01:26:08 -0500

    Completed Input and Output Challenge
    Implemented in C++ - Full Points

---
+++ C-C++/C++-Domain/Introduction/Input-and-Output/solution.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int sum = 0;
+    int temp;
+    cin>>temp;
+    sum+=temp;
+    cin>>temp;
+    sum+=temp;
+    cin>>temp;
+    sum+=temp;
+    cout<<sum<<endl;
+    return 0;
+}
